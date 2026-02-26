e3811e44c5ef9b1f5e519745df468845dfe1adbd
Irfan Arifin <ir.irfan.arifin@gmail.com>
2026-02-26 16:28:35 +0700

    refactor: Simplify footer layout by removing review badges and updating subscription form structure; clean up .gitignore for image assets

--- resources/views/inc/main/footer.blade.php
+++ resources/views/inc/main/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container pt-5">
-    <div class="row align-items-center">
+    <div class="row">
         <div class="col-md-4">
             <div class="address p-md-0 p-4">
                 <div class="title">
@@ -28,40 +28,19 @@
                 </ul>
             </div>
         </div>
-        <div class="col-md-4 d-flex align-items-center justify-content-center">
-            <div class="review-badges p-md-0 p-4 text-center w-100">
-                <div class="title mb-3">
-                    Our Reviews
-                </div>
-                <div class="d-flex flex-row align-items-center justify-content-center gap-3">
-                    <div class="badge-item">
-                        <a href="https://www.google.com/search?q=prasana+by+arjani+resorts&oq=prasana+by+&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIGCAEQRRg5MgcIAhAAGIAEMgcIAxAAGIAEMgcIBBAAGIAEMgYIBRBFGEEyBggGEEUYPDIGCAcQRRg80gEIMjM0NmowajeoAgCwAgA&sourceid=chrome&ie=UTF-8"
-                           target="_blank" rel="noopener noreferrer">
-                            <img src="{{ asset('/img/google-review.png') }}" class="img-fluid" alt="Google 4.7 Star Rating"
-                                style="max-width: 140px; max-height: 70px; object-fit: contain;">
-                        </a>
-                    </div>
-                    <div class="badge-item">
-                        <a href="https://www.tripadvisor.com.au/Hotel_Review-g1219108-d9623334-Reviews-Prasana_Villas_By_Arjani_Resorts-Ungasan_Bukit_Peninsula_Bali.html"
-                           target="_blank" rel="noopener noreferrer">
-                            <img src="{{ asset('/img/tripadvisor-award.png') }}" class="img-fluid"
-                                alt="Tripadvisor Travellers Choice Awards Best of the Best 2026"
-                                style="max-width: 100px; max-height: 100px; object-fit: contain;">
-                        </a>
-                    </div>
-                </div>
+        <div class="col-md-8">
+            <div class="subscribe-title">
+                Don’t miss our update.
+                Subscribe us for more info
             </div>
-        </div>
-        <div class="col-md-4">
-            <div class="p-md-0 p-4">
-                <div class="subscribe-title" style="font-size: 1.25rem; line-height: 1.6; margin-left: 0; margin-bottom: 1.5rem;">
-                    Don’t miss our update.<br>Subscribe us for more info
+            <form class="row form-subscribe">
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="subscribe" placeholder="Enter your email address">
                 </div>
-                <form class="form-subscribe" style="margin-left: 0;">
-                    <input type="text" class="form-control mb-3" id="subscribe" placeholder="Enter your email address">
+                <div class="col-md-4 mt-md-0 mt-3">
                     <button type="submit" class="btn btn-subscribe mb-3">Subscribe</button>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
